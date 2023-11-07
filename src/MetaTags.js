20a9114f44daa7b01ac7d444614cd7b09b1a148d
Jumaru <spiralout.eu@gmail.com>
2023-11-07 23:36:32 +0200

    Fix meta image ref

--- src/MetaTags.js
+++ src/MetaTags.js
@@ -8,19 +8,19 @@ const MetaTags = () => {
       <meta name="description" content="Generate Ethereum wallets securely in your browser. No data is stored or transmitted." />
       <meta name="keywords" content="Ethereum, Wallet, Generator, Blockchain, Cryptocurrency" />
       <meta name="author" content="Jumaru" />
-      <link rel="canonical" href="https://spiraloutdoteu.github.io/ethereum-wallet-generator/" />
+      <link rel="canonical" href="https://ethereum-wallet-generator-online.vercel.app/" />
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://spiraloutdoteu.github.io/ethereum-wallet-generator/" />
-      <meta property="og:title" content="Ethereum Wallet Generator" />
+      <meta property="og:url" content="https://ethereum-wallet-generator-online.vercel.app/" />
+      <meta property="og:title" content="Ethereum Wallet Generator Online" />
       <meta property="og:description" content="Generate Ethereum wallets securely in your browser. No data is stored or transmitted." />
-      <meta property="og:image" content="demo.png" />
+      <meta property="og:image" content="%PUBLIC_URL%/demo.png" />
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://spiraloutdoteu.github.io/ethereum-wallet-generator/" />
-      <meta property="twitter:title" content="Ethereum Wallet Generator" />
+      <meta property="twitter:url" content="https://ethereum-wallet-generator-online.vercel.app/" />
+      <meta property="twitter:title" content="Ethereum Wallet Generator Online" />
       <meta property="twitter:description" content="Generate Ethereum wallets securely in your browser. No data is stored or transmitted." />
-      <meta property="twitter:image" content="demo.png" />
+      <meta property="twitter:image" content="%PUBLIC_URL%/demo.png" />
     </Helmet>
   );
 };
