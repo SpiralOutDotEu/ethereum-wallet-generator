ec6733fd926a63e14ca39a0ecc5b04c1f122830f
Jumaru <spiralout.eu@gmail.com>
2023-11-07 20:58:40 +0200

    Format style with tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ethereum Wallet Generator</h1>
+      <h1 className="text-4xl font-bold mb-4">
+        Ethereum Wallet Generator
+      </h1>
         <WalletGenerator />
       </header>
     </div>
--- src/WalletGenerator.js
+++ src/WalletGenerator.js
@@ -2,29 +2,44 @@ import React, { useState } from 'react';
 import { ethers } from 'ethers';
 
 const WalletGenerator = () => {
-  const [walletInfo, setWalletInfo] = useState(null);
+    const [walletInfo, setWalletInfo] = useState(null);
 
-  const createWallet = () => {
-    const wallet = ethers.Wallet.createRandom();
-    setWalletInfo({
-      address: wallet.address,
-      mnemonic: wallet.mnemonic.phrase,
-      privateKey: wallet.privateKey
-    });
-  };
+    const createWallet = () => {
+        const wallet = ethers.Wallet.createRandom();
+        setWalletInfo({
+            address: wallet.address,
+            mnemonic: wallet.mnemonic.phrase,
+            privateKey: wallet.privateKey
+        });
+    };
 
-  return (
-    <div>
-      <button onClick={createWallet}>Create Wallet</button>
-      {walletInfo && (
-        <div>
-          <p>Address: {walletInfo.address}</p>
-          <p>Mnemonic: {walletInfo.mnemonic}</p>
-          <p>Private Key: {walletInfo.privateKey}</p>
-        </div>
-      )}
-    </div>
-  );
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center px-4 py-6">
+
+                <p className="text-gray-600 mb-4 text-center mx-2">
+                    Click the button below to generate a new Ethereum wallet. All operations are done locally in your browser; we never store or transmit your private information.
+                </p>
+                <button
+                    onClick={createWallet}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 w-full sm:w-auto"
+                >
+                    Create Wallet
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center px-4 py-6">
+                {walletInfo && (
+                    <div className="mt-4 p-4 w-full bg-white rounded-lg border shadow-md sm:max-w-md sm:mx-0 mx-2 overflow-auto">
+                        <div className="overflow-x-auto">
+                            <p className="text-gray-700 text-base"><b>Address:</b> {walletInfo.address}</p>
+                            <p className="text-gray-700 text-base mt-2"><b>Mnemonic:</b> {walletInfo.mnemonic}</p>
+                            <p className="text-gray-700 text-base mt-2"><b>Private Key:</b> <span className="break-all">{walletInfo.privateKey}</span></p>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default WalletGenerator;
