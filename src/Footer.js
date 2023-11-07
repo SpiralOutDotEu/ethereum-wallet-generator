256b3cf4bcee3a84396a6d9372fa4abc7a707937
Jumaru <spiralout.eu@gmail.com>
2023-11-08 00:08:34 +0200

    Simplify twitter link

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                     Give a Star on GitHub
                 </a>
                 <div>
-                <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fpublish.twitter.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5Ejumaru_igano&screen_name=jumaru_igano"
+                <a href="https://twitter.com/intent/follow?jumaru_igano&screen_name=jumaru_igano"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="inline-block bg-gray-700 hover:bg-gray-600 text-white font-semibold py-2 px-4 border border-gray-600 rounded shadow text-sm">
