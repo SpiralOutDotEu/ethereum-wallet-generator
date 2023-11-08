cab28b0c7a3f4ddb0f2295e26561fc946e036692
Jumaru <spiralout.eu@gmail.com>
2023-11-08 08:54:04 +0200

    Remove metatags from App and put in Index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Footer from './Footer';
 import WalletGenerator from './WalletGenerator';
-import MetaTags from './MetaTags';
 
 function App() {
   return (
     <div className="App">
-      <MetaTags />
       <header className="App-header">
       <h1 className="text-4xl font-bold mb-4">
         Ethereum Wallet Generator
