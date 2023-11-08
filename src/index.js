2906a8ee5e22bcd47d5c76b28e4331e07a5a0dbc
Jumaru <spiralout.eu@gmail.com>
2023-11-08 09:17:54 +0200

    Simplify metatags

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MetaTags from './MetaTags';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MetaTags />
+    <Helmet>
+      <title>Ethereum Wallet Generator</title>
+      <meta name="description" content="Ethereum Wallet Generator" />
+      <meta name="title" key="title" content="Ethereum Wallet Generator" />
+      <meta property="og:title" key="og:title" content="Ethereum Wallet Generator" />
+      <meta property="og:locale" key="og:locale" content="en_US" />
+      <meta charSet="utf-8" />
+      <meta property="og:type" key="og:type" content="website" />
+      <meta
+        property="og:description"
+        key="og:description"
+        content="Generate Ethereum wallets securely in your browser. No data is stored or transmitted."
+      />
+      <meta
+        property="og:image"
+        key="og:image"
+        content="https://ethereum-wallet-generator-online.vercel.app/demo.png"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
